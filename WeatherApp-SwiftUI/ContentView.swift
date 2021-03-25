c87cc00fa81fa9f371611e7b17813c285b9022a4
Piers Thompson <pthom198@ford.com>
2021-03-25 21:57:48 +0000

    added WeatherDayView Struct and button for Night Mode

--- WeatherApp-SwiftUI/ContentView.swift
+++ WeatherApp-SwiftUI/ContentView.swift
@@ -24,16 +24,34 @@ struct ContentView: View {
                 Text("8°")
                     .font(.system(size: 80, weight: .medium, design: .default))
                     .foregroundColor(.white)
-                Spacer()
+                    .padding(.bottom, 80)
                 HStack(spacing: 18) {
-                    WeatherDayView(time: "Now", imageName: "cloud.fill", temperature: 8)
-                    WeatherDayView(time: "18", imageName: "cloud.fill", temperature: 7)
-                    WeatherDayView(time: "19", imageName: "cloud.fill", temperature: 7)
-                    WeatherDayView(time: "20", imageName: "cloud.fill", temperature: 6)
-                    WeatherDayView(time: "21", imageName: "cloud.fill", temperature: 6)
-                    WeatherDayView(time: "22", imageName: "cloud.fill", temperature: 6)
-                    WeatherDayView(time: "23", imageName: "cloud.fill", temperature: 6)
-                    WeatherDayView(time: "24", imageName: "cloud.fill", temperature: 6)
+                    WeatherTimeView(time: "Now", imageName: "cloud.fill", temperature: 8)
+                    WeatherTimeView(time: "18", imageName: "cloud.fill", temperature: 7)
+                    WeatherTimeView(time: "19", imageName: "cloud.fill", temperature: 7)
+                    WeatherTimeView(time: "20", imageName: "cloud.fill", temperature: 6)
+                    WeatherTimeView(time: "21", imageName: "cloud.rain.fill", temperature: 6)
+                    WeatherTimeView(time: "22", imageName: "cloud.rain.fill", temperature: 6)
+                    WeatherTimeView(time: "23", imageName: "cloud.rain.fill", temperature: 6)
+                    WeatherTimeView(time: "24", imageName: "cloud.fill", temperature: 6)
+                }
+
+                VStack() {
+                WeatherDayView(day: "Monday", imageName: "cloud.sun.fill", dayTemperature: 11, nightTemperature: 10)
+                WeatherDayView(day: "Tuesday", imageName: "cloud.sun.fill", dayTemperature: 11, nightTemperature: 10)
+                WeatherDayView(day: "Wednesday", imageName: "cloud.sun.fill", dayTemperature: 11, nightTemperature: 10)
+                WeatherDayView(day: "Thursday", imageName: "cloud.sun.fill", dayTemperature: 11, nightTemperature: 10)
+                WeatherDayView(day: "Friday", imageName: "cloud.sun.fill", dayTemperature: 11, nightTemperature: 10)
+                }
+                Spacer()
+                Button {
+                    print("tapped")
+                } label: {
+                    Text("Night Mode")
+                        .frame(width: 200, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .font(.system(size: 24, weight: .bold, design: .default))
+                        .background(Color.black)
+                        .cornerRadius(10)
                 }
                 Spacer()
             }
@@ -47,7 +65,7 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct WeatherDayView: View {
+struct WeatherTimeView: View {
     var time: String
     var imageName: String
     var temperature: Int
@@ -64,6 +82,35 @@ struct WeatherDayView: View {
             Text("\(temperature)°")
                 .font(.system(size: 28, weight: .medium, design: .default))
                 .foregroundColor(.white)
+        }
+    }
+}
+
+struct WeatherDayView: View {
+    var day: String
+    var imageName: String
+    var dayTemperature: Int
+    var nightTemperature: Int
+    var body: some View {
+        HStack(){
+            Text(day)
+                .font(.system(size: 20, weight: .medium, design: .default))
+                .foregroundColor(.white)
+                .frame(width: 180, height: 30, alignment: .leading)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 30, height: 30)
+            HStack(spacing: 20){
+            Text("\(dayTemperature)")
+                .font(.system(size: 20, weight: .medium, design: .default))
+                .foregroundColor(.white)
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 30, alignment: .trailing)
+            Text("\(nightTemperature)")
+                .font(.system(size: 20, weight: .medium, design: .default))
+                .foregroundColor(.gray)
+            }
         }
     }
 }
